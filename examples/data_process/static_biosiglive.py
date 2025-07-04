96ca293bb6ee0ca987a35955fb27bb6fbcce62da
ipuch <pierre.puchaud@umontreal.ca>
2025-07-04 11:24:16 -0400

    process auto force

--- examples/data_process/static_biosiglive.py
+++ examples/data_process/static_biosiglive.py
@@ -1,7 +1,10 @@
+import pandas as pd
+
 from biosiglive import MskFunctions
 from pathlib import Path
 import matplotlib.pyplot as plt
 import pickle
+import ast
 
 import numpy as np
 import biorbd
@@ -80,9 +83,7 @@ def n_iter(n):
     return act, res, tau_list
 
 def load_model(model_path):
-    current_file_dir = Path(__file__).parent
-
-    model = biorbd.Model(f"{current_file_dir}/../model_msk/{model_path}")
+    model = biorbd.Model(model_path)
 
     return model
 
@@ -101,7 +102,7 @@ def load_data(data_path):
         dof_name="r_ulna_radius_hand_r_elbow_flex_RotX",
     )
     converter.get_data_at_handle()
-    data_dict = converter.dictionary
+    data_dict = converter.handle_dictionary
 
     return data_dict
 
@@ -131,31 +132,97 @@ def get_muscles_forces_from_inv_dynamics(data_path, model_path):
 
     return force_biclong, force_bicshort, tau_list, act
 
-def get_muscles_forces(data_path, model_path):
-    current_file_dir = Path(__file__).parent
-    model = load_model(model_path)
-    data_dict = load_data(data_path)
+def save_in_pkl(data, file_path):
+    with open(file_path, "wb") as f:
+        pickle.dump(data, f)
 
-    msk_functions = MskFunctions(model=f"{current_file_dir}/../model_msk/{model_path}")
+
+def get_muscles_forces(p_n, data_path, biomodel_path, save=True):
+    current_file_dir = Path(__file__).parent
+    whole_biomodel_path = "/home/mickaelbegon/Documents/Stage_Florine/modelScaled/" + biomodel_path
+    model = load_model(whole_biomodel_path)
+    whole_data_path = "/home/mickaelbegon/Documents/Stage_Florine/Data/" + data_path
+    data_dict = load_data(whole_data_path)
+    time = data_dict["time"]
+    stim_time = data_dict["stim_time"]
+
+    msk_functions = MskFunctions(model=whole_biomodel_path)
+
+    p_data = pd.read_csv("/home/mickaelbegon/Documents/Stage_Florine/Data/data_participants.csv", sep=";")
+    d_handle_rotation = p_data.iloc[p_n - 1]["handle_elbow_dist"] / 100 # Convert cm to m
 
     tau_list = []
 
-    for i in range(len(data_dict["time"][-1])):
-        Q = np.array([[90.]])
-        Qdot = np.array([[0.]])
-        Tau = np.array([[0.23 * data_dict["x"][-1][i]]])
-        tau_list.append(Tau[0][0])
-        act, res = msk_functions.compute_static_optimization(Q, Qdot, Tau, use_residual_torque=True)
-
-    max_forces = [624.29999999999995, 435.56]
+    max_forces = [model.muscle(0).characteristics().forceIsoMax(), model.muscle(1).characteristics().forceIsoMax()]
     force_biclong = []
     force_bicshort = []
+
+    for j in range(len(data_dict["time"])):
+        for i in range(len(data_dict["time"][j])):
+            Q = np.array([[90.]])
+            Qdot = np.array([[0.]])
+            norm = np.sqrt(data_dict["x"][j][i]**2 + data_dict["y"][j][i]**2 + data_dict["z"][j][i]**2)
+            Tau = np.array([[d_handle_rotation * norm]])
+            tau_list.append(Tau[0][0])
+            act, res = msk_functions.compute_static_optimization(Q, Qdot, Tau, use_residual_torque=True)
 
     for i in range(len(res[0])):
         force_bicshort.append(act[1][i] * max_forces[1])
         force_biclong.append(act[0][i] * max_forces[0])
 
-    return force_biclong, force_bicshort, tau_list, act
+
+    dict = {"time": time, "stim_time": stim_time, "force_biclong": force_biclong, "force_bicshort": force_bicshort, "tau_list": tau_list, "act": act, "res": res}
+    if save:
+        save_path = data_path[4:-3] + "pkl"
+        saving_path = f"{current_file_dir}/pkl_files/{save_path}"
+        save_in_pkl(dict, saving_path)
+
+    return dict
+
+def slicing(data, time):
+    first = 0
+    sliced_data = []
+    for i in range(len(time)):
+        last = first + len(time[i])
+        sliced_data.append(data[first:last])
+        first = last
+    return sliced_data
+
+def auto_process(p_n_list, save=True, plot=False):
+    p_data = pd.read_csv("/home/mickaelbegon/Documents/Stage_Florine/Data/data_participants.csv", sep=";")
+    for p_n in p_n_list:
+        freq_str = p_data.iloc[p_n - 1]["freq_force"]
+        freq_list = ast.literal_eval(freq_str)
+        seeds_str = p_data.iloc[p_n - 1]["seed_force"]
+        seeds_list = ast.literal_eval(seeds_str)
+
+        for i in range(len(freq_list)):
+            p_nb = str(p_n) if len(str(p_n)) == 2 else "0" + str(p_n)
+            data_path = "P" + p_nb + "/p" + p_nb + "_force_" + str(freq_list[i]) + "Hz_" + str(seeds_list[i]) + ".c3d"
+            print(data_path)
+            model_path = "p" + p_nb + "_scaling_scaled.bioMod"
+            dict = get_muscles_forces(p_n=p_n, data_path=data_path, biomodel_path=model_path, save=save)
+            if plot:
+                time = dict["time"]
+                stim_time = dict["stim_time"]
+                force_biclong = slicing(dict["force_biclong"], time)
+                force_bicshort = slicing(dict["force_bicshort"], time)
+
+                for j in range(len(time)):
+                    if j==0:
+                        plt.plot(time[j], force_biclong[j], label="Force BIC Long", color="blue")
+                        plt.plot(time[j], force_bicshort[j], label="Force BIC Short", color="orange")
+                        plt.scatter(stim_time[j], [0] * len(stim_time[j]), color="green", label="Stimulations")
+                    else:
+                        plt.plot(time[j], force_biclong[j], color="blue")
+                        plt.plot(time[j], force_bicshort[j], color="orange")
+                        plt.scatter(stim_time[j], [0] * len(stim_time[j]), color="green")
+                plt.title(f"Participant {p_n} - Forces")
+                plt.xlabel("Time (s)")
+                plt.ylabel("Force (N)")
+                plt.legend()
+                plt.show()
+
 
 def result(n):
     act_n, res_n, tau_n = n_iter(n)
@@ -189,36 +256,4 @@ def result(n):
     plt.show()
 
 if __name__ == "__main__":
-    force_biclong_inv_dy, force_bishort_inv_dy, tau_inv_dy, act_inv_dy = get_muscles_forces_from_inv_dynamics(
-        data_path="/home/mickaelbegon/Documents/Stage_Florine/tau_p07.pkl",
-        model_path="arm26_allbiceps_1dof.bioMod")
-
-
-    force_biclong, force_bishort, tau, act = get_muscles_forces(data_path="/home/mickaelbegon/Documents/Stage_Florine/p07_force_50Hz_19.c3d",
-                       model_path="arm26_allbiceps_1dof.bioMod")
-
-    fig, axs = plt.subplots(2, 3, figsize=(10, 8))
-    axs[0, 0].plot(tau, label="tau from static")
-    axs[0, 0].plot(tau_inv_dy, label="tau from inverse dynamics")
-    axs[0, 0].set_title("Tau")
-    axs[0, 0].legend()
-    axs[0, 1].plot(force_biclong, label="Force BIC long from static")
-    axs[0, 1].plot(force_biclong_inv_dy, label="Force BIC long from inverse dynamics")
-    axs[0, 1].set_title("Force BIC long")
-    axs[0, 1].legend()
-    axs[1, 1].plot(force_bishort, label="Force BIC short from static")
-    axs[1, 1].plot(force_bishort_inv_dy, label="Force BIC short from inverse dynamics")
-    axs[1, 1].set_title("Force BIC short")
-    axs[1, 1].legend()
-    axs[0, 2].plot(act[0], label="Activation BIC long from static")
-    axs[0, 2].plot(act_inv_dy[0], label="Activation BIC long from inverse dynamics")
-    axs[0, 2].set_title("Activation BIC long")
-    axs[0, 2].legend()
-    axs[1, 2].plot(act[1], label="Activation BIC short from static")
-    axs[1, 2].plot(act_inv_dy[1], label="Activation BIC short from inverse dynamics")
-    axs[1, 2].set_title("Activation BIC short")
-    axs[1, 2].legend()
-
-    plt.show()
-
-    #print(force_biclong)
+    auto_process([5], save=False, plot=True)
